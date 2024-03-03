cbff25f5b44cacabb45bafac74e4fef95a4f7064
cianr <cianr@MSI>
2024-03-04 08:38:06 +1100

    Add project files.

---
+++ ButterflyWebApplication1/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ButterflyWebApplication1.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ ButterflyWebApplication1/Controllers/MathController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace ButterflyWebApplication1.Controllers
+{
+    public class MathController : ApiController
+    {
+        [HttpPost]
+        [HttpGet]
+        [ActionName("Calculate")]
+        public IHttpActionResult Calculate([FromBody] string expression)
+        {
+            try
+            {
+                double result = CalculateExpression(expression);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error evaluating expression: {ex.Message}");
+            }
+        }
+
+        private double CalculateExpression(string expression)
+        {
+            DataTable tempTable = new DataTable();
+            var result = tempTable.Compute(expression, "");
+            return Convert.ToDouble(result);
+        }
+    }
+}
